e472fc2a407c0d3a2a2c79077ba1e6d24cd63452
Anurag Dubey <AnuragDubeyGames@gmail.com>
2023-07-13 19:30:08 +0530

    Scraping Fact And Img URL - Done

--- FACTBOT_A_YT_PROJECT/FactsScraper.py
+++ FACTBOT_A_YT_PROJECT/FactsScraper.py
@@ -1,30 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+website_url = 'https://www.mentalfloss.com/amazingfactgenerator'
+data_dictionary = {}
 
+wait = WebDriverWait(driver, 10)
 
 def scrape_website(url):
     driver.get(url)
-    driver.implicitly_wait(0.5)
 
-    text_box = driver.find_element(by=By.CLASS_NAME, value="_qsor55")
-    
-    imgResult = driver.find_element(By.XPATH,'//*[@id="mm-facts-widget-inner"]/article/div/div/figure/div/div')
-    image_url = imgResult.get_attribute('innerHTML')
+    text_box = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, '_qsor55')))
+    img_element = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mm-facts-widget-inner"]/article/div/div/figure/div/div')))
+    image_html = img_element.get_attribute('innerHTML')
 
-    split_text = image_url.split()
+    split_text = image_html.split()
     src_item = next(item for item in split_text if item.startswith('src='))
-    imgUrl = src_item.split('"')[1]
+    img_url = src_item.split('"')[1]
 
-    return text_box.text, imgUrl
+    return text_box.text, img_url
 
-website_url = 'https://www.mentalfloss.com/amazingfactgenerator'  
-scraped_text, scraped_url = scrape_website(website_url)
+N = 0
+for _ in range(N):
+    scraped_text, scraped_url = scrape_website(website_url)
+    data_dictionary[scraped_text] = scraped_url
 
-data_dictionary = {
-    scraped_text: scraped_url
-}
-
-print(data_dictionary)
 driver.quit()
+
+
+
+
