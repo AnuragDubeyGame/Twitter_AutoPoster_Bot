97a03f24b06551b845a2c8e22678004532734956
AnuragDubey <124233582+AnuragDubeyGame@users.noreply.github.com>
2023-07-15 16:54:54 +0530

    Uploading data to DB = DONE

--- FACTBOT_A_YT_PROJECT/FactsScraper.py
+++ FACTBOT_A_YT_PROJECT/FactsScraper.py
@@ -2,19 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pymongo import MongoClient
 
 
-
+ROWS_TO_FETCH = 50 # Just Enough for 1 years of tweet.
 data_dictionary = {}
-ROWS_TO_FETCH = 5500 # Just Enough for 1 years of tweet.
-
-
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 1)
+wait = WebDriverWait(driver, 10)
 website_url = 'https://www.mentalfloss.com/amazingfactgenerator'
+DBurl = "mongodb+srv://factboyuniverse:factboytestpass@factsdatabasecluster.ej0bjql.mongodb.net/"
+
+
+def PostDataToDB(f,uri):
+
+   data = {
+        'Facts': f,
+        'ImageURL': uri
+    }
+   result = collection.insert_one(data)
+
+print("STARTED SCRAPING...")
+
 def scrape_website(url):
-    driver.get(url);
+    driver.get(url)
 
     text_box = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, '_qsor55')))
     img_element = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mm-facts-widget-inner"]/article/div/div/figure/div/div')))
@@ -25,11 +36,22 @@ def scrape_website(url):
     img_url = src_item.split('"')[1]
 
     return text_box.text, img_url
+
 for i in range(ROWS_TO_FETCH):
     scraped_text, scraped_url = scrape_website(website_url)
     data_dictionary[scraped_text] = scraped_url
+
+print("SCRAPED SUCCESFULLY!")
+
+client = MongoClient(DBurl)
+db = client['FactsDB']
+collection = db['factsCollection']
+
+print("POSTING ",len(data_dictionary),"FACTS ON DB")
+for key, value in data_dictionary.items():
+    PostDataToDB(key,value)
+
+print("POSTED FACTS ON DB SUCCESSFULLY!")
+
+client.close()
 driver.quit()
-
-
-
-
