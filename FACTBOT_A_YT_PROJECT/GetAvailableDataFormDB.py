f179f565f86cc3ec895dbcbf9d4ae57698d88005
AnuragDubey <124233582+AnuragDubeyGame@users.noreply.github.com>
2023-07-17 15:39:51 +0530

    Added a new file

---
+++ FACTBOT_A_YT_PROJECT/GetAvailableDataFormDB.py
@@ -0,0 +1,20 @@
+import requests
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+DBurl = "mongodb+srv://factboyuniverse:factboytestpass@factsdatabasecluster.ej0bjql.mongodb.net/"
+
+
+def main():
+   print("\t\t Fetching Facts From DB... \t\t")
+   client = MongoClient(DBurl)
+   db = client['FactsDB']
+   collection = db['factsCollection']
+   collection.find()
+   data_count = collection.count_documents({})
+   print("AVAILABLE DATA IN DB:", data_count)
+   client.close()
+   
+
+main()
